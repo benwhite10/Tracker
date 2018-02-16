88ae53eb9fcfc9a9a4bb911348f73a28ab64d696
benwhite10 <ben.white10@outlook.com>
2018-02-16 14:00:06 +0000

    Tidy up upload front end

--- public_html/js/feed.js
+++ public_html/js/feed.js
@@ -2,14 +2,27 @@ $(document).ready(function(){
 	checkLoginStatus();
 });
 
+function setUpFeed() {
+    addNameToHeader();
+	setUpUploadButton();
+	requestActivity();
+}
+
 function setUpUploadButton() {
-	$("#upload_form").submit(function(event) {
-		event.preventDefault();
-		$("#upload_button").html('Uploading...');
+	$("#file_select").change(function () {
+		path = $("#file_select").val();
+		split = path.split("\\");
+		name = split[split.length - 1];
+	   $("#file_display").html(name);
+	});
 
+	$("#file_display").click(function() {
+		$("#file_select").click();
+	})
+
+	$("#submit_button").click(function() {
+		var formData = new FormData(document.getElementById("upload_form"));
 		user = JSON.parse(localStorage.user);
-
-		var formData = new FormData(this);
 		formData.append("userid", user["ID"]);
 
 		$.ajax({
@@ -20,15 +33,24 @@ function setUpUploadButton() {
 			cache: false,
 			processData: false,
 	        success: function(data) {
-				console.log(data);
-				$("#file_select").val("");
+				uploadSuccess(JSON.parse(data));
 	        },
 	        error: function(data) {
 				console.log("Failure");
-				console.log(data);
+				console.log(json);
 	        }
 		});
 	});
+}
+
+function uploadSuccess(json) {
+	if (json["success"]) {
+		$("#file_select").val("");
+		$("#file_display").html("Click here to upload file...");
+		requestActivity();
+	} else {
+		console.log(json["message"]);
+	}
 }
 
 function checkLoginStatus() {
@@ -48,8 +70,7 @@ function checkLoginStatus() {
         dataType: "json",
         success: function(json) {
             if (json["success"] && json["result"]["ValidSession"]) {
-                addNameToHeader();
-				setUpUploadButton();
+				setUpFeed();
             } else {
                 console.log("Bad");
                 localStorage.user = "";
@@ -63,6 +84,58 @@ function checkLoginStatus() {
     });
 }
 
+function requestActivity() {
+	user = JSON.parse(localStorage.user);
+	var data = {
+        type: "GETACTIVITY",
+        userid: user["ID"],
+        session: user["SessionID"]
+    };
+    $.ajax({
+        type: "POST",
+        data: data,
+        url: "requests/activity.php",
+        dataType: "json",
+        success: function(json) {
+            requestActivitySuccess(json);
+        },
+        error: function(json) {
+            console.log(json);
+        }
+    });
+}
+
+function requestActivitySuccess(json) {
+	if (json["success"]) {
+		parseActivities(json["result"]);
+	} else {
+		console.log("There was an error retrieving your activity.");
+		console.log(json["message"]);
+	}
+}
+
+function parseActivities(activities) {
+	activities_html = "";
+	for (i = 0; i < activities.length; i++) {
+		activity = activities[i];
+		activities_html += activityHTML(activity);
+	}
+	$("#activity_feed_container").html(activities_html);
+}
+
+function activityHTML(activity) {
+	date = moment(activity["Date"]);
+	date_string = date.format("DD/MM/YY");
+	html = "<div class='activity'>";
+	html += "<div class='activity_top_container'>";
+	html += "<div class='activity_title'>" + activity["Name"] + "</div>";
+	html += "<div class='activity_date'>" + date_string + "</div>";
+	html += "</div>";
+	html += "<div class='activity_main'></div>";
+	html += "</div>";
+	return html;
+}
+
 function addNameToHeader() {
     user = JSON.parse(localStorage.user);
     $("#header_name").html(user["FirstName"] + " " + user["Surname"]);
@@ -70,4 +143,9 @@ function addNameToHeader() {
 
 function clickName() {
 	window.location.href = "/login.html";
+}
+
+function clickAddGPS() {
+	display = $("#add_gps_container").css("display");
+	$("#add_gps_container").css("display", display === "block" ? "none" : "block");
 }
